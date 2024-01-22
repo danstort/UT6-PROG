528c501f49c7e0dc13d6cd1804a2faecfdf51940
danielsotoortiz <danielsotoortiz@macbook-air.alumnado.educarm.net>
2024-01-22 08:28:14 +0100

    First version

---
+++ src/POOEj1H1/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package POOEj1H1;
+
+/**
+ *
+ * @author danielsotoortiz
+ */
+public class Main {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
