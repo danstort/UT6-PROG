05687181c9aaac99367511bedf9fa6f2b841db9b
daniel soto <daniel.soto.ortiz@gmail.com>
2024-03-03 10:09:16 +0100

    d

--- src/POOEj1H6/Persona.java
+++ src/POOEj1H6/Persona.java
@@ -1,5 +1,38 @@
 package POOEj1H6;
 
+import java.util.Scanner;
+
 public class Persona {
+
+    Scanner tec = new Scanner(System.in);
+    int edad;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public Persona(int edad) {
+        this.edad = edad;
+    }
+
+    public Persona() {
+
+    }
+
+    public String lee() {
+
+        String t = tec.nextLine();
+        return t;
+    }
+
+    public int divide(int a, int b){
+
+        return a/b;
+    }
+
 
 }
--- src/POOEj1H6/PersonaMain.java
+++ src/POOEj1H6/PersonaMain.java
@@ -1,11 +1,17 @@
 package POOEj1H6;
 
 
+import java.util.Scanner;
+
 public class PersonaMain {
 
     
     public static void main(String[] args) {
-        
+
+        Scanner tec = new Scanner(System.in);
+
+        Persona p1 = new Persona(3);
+        int a = Integer.paseInt(p1.lee());
     }
 
 }
